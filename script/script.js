60cb92e192f05f1e0efec78273e32c450644ed13
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-13 17:46:20 +0600

    trying to do something

--- script/script.js
+++ script/script.js
@@ -1,27 +1,3 @@
-function calculateTriangleArea() {
-    const base = getInputFromId('inputBase');
-    const height = getInputFromId('inputHeight')
-
-    const area = 0.5 * base * height;
-
-    showResult('result', area);
-}
-
-
-
-
-
-function getInputFromId(inputFieldId) {
-    const inputField = document.getElementById(inputFieldId).value;
-    const base = parseFloat(inputField);
-    return base;
-}
-
-
-function showResult(takeShowId, result) {
-    const showId = document.getElementById(takeShowId);
-    showId.innerText = result;
-}
 
 
 
